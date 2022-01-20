341be64e37fe7360a4005e9b8ff95a4e12e626eb
Nami Sunami <naoyuki.sunami@gmail.com>
2022-01-20 10:31:24 -0500

    Join downloaded data from HUD

--- src/download_hud_datasets.R
+++ src/download_hud_datasets.R
@@ -2,12 +2,23 @@
 
 library(tidyverse)
 library(here)
+library(readxl)
 
 hud_2020_tract_AK_URL <- "https://www.huduser.gov/portal/datasets/pictures/files/TRACT_AK_MN_2020.xlsx"
 hud_2020_tract_MO_URL <- "https://www.huduser.gov/portal/datasets/pictures/files/TRACT_MO_WY_2020.xlsx"
+hud_2020_tract_AK_filepath <- here("data/raw/", "TRACT_AK_MN_2020.xlsx")
+hud_2020_tract_MO_filepath <- here("data/raw/", "TRACT_MO_WY_2020.xlsx")
 
-download.file(hud_2020_tract_AK_URL,
-              here("data/raw/TRACT_AK_MN_2020.xlsx"))
-download.file(hud_2020_tract_MO_URL,
-              here("data/raw/TRACT_MO_WY_2020.xlsx"))
 
+download.file(hud_2020_tract_AK_URL, hud_2020_tract_AK_filepath)
+download.file(hud_2020_tract_MO_URL, hud_2020_tract_MO_filepath)
+
+AK_data <- read_xlsx(hud_2020_tract_AK_filepath)
+MO_data <- read_xlsx(hud_2020_tract_MO_filepath)
+
+joined_data <- AK_data %>% 
+    bind_rows(MO_data)
+
+# Save file
+write_csv(joined_data, here("data/processed/hud_2020_tract_joined.csv"))
+write_rds(joined_data, here("data/processed/hud_2020_tract_joined.rds"))
