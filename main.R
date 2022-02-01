24c35fc8e6bfaa5f041ae27b95aed2ec52280ca5
Nami Sunami <naoyuki.sunami@gmail.com>
2022-02-01 13:00:20 -0500

    Create main.R

---
+++ main.R
@@ -0,0 +1,7 @@
+# This file describes which R scripts should be run in which order
+# to rebuild the project processed datasets
+library(here)
+source(here("src/download_acs_data.R"))
+source(here("src/download_hud_datasets.R"))
+source(here("src/join_acs_hud.R"))
+source(here("src/add_geometry_to_ACS_HUD.R"))
