af507c83750c7e8342d560b036429efebe6568e0
mehak25 <47729138+mehak25@users.noreply.github.com>
2022-02-02 15:36:19 -0500

    Integrating visuals to app

--- app/advocates_panel.r
+++ app/advocates_panel.r
@@ -7,11 +7,5 @@ advocates_panel <- tabPanel(
              "Find Out How Your Neighbohood is Doing"),
     tags$div(class = "center-container",
              uiOutput("GEOID_selector")),
-    tags$div(class = "main-point",
-             "In the selected X census tracts, only XX% of the families
-             needing Housing Vouche Voucher are receiving it."),
-    tags$div(class = "comment",
-             "(*Add a pie chart for the overview? receiving vs not receiving)"),
-    tags$div(class = "comment",
-             "(*Add the table showing reported & eligible households)")
+    tableOutput("advoc_table"),
 )
