b6521cfe44586314645bbcd4d9ef9bb784a36a29
mehak25 <47729138+mehak25@users.noreply.github.com>
2022-02-02 17:52:21 -0500

    Integrating visuals to app

--- app/server.R
+++ app/server.R
@@ -1,8 +1,27 @@
-library(shiny)
-library(tidyverse)
-library(plotly)
-library(sf)
-library(here)
+
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(
+    here,
+    htmltools,
+    tippy,
+    xml2,
+    tibble, 
+    rvest, 
+    stringr, 
+    tidyr, 
+    janitor, 
+    dplyr, 
+    pdftools, 
+    readr, 
+    leaflet, 
+    tigris, 
+    tidyverse, 
+    shiny, 
+    ggpubr, 
+    plotly, 
+    lubridate, 
+    RColorBrewer,peRspective,reshape,plotly,sf)
+
 
 # Load Data
 geo_data <- read_rds("acs_hud_de_geojoined.rds")
