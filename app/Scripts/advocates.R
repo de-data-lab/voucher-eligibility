338dc07dc1bf2f1b9d4cbf5fa4decb6153f9aca5
mehak25 <47729138+mehak25@users.noreply.github.com>
2022-02-07 11:03:56 -0500

    Worked on pending issues

---
+++ app/Scripts/advocates.R
@@ -0,0 +1,24 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(sf)
+
+# Load Data
+acs_hud_de_geojoined <- read_rds("acs_hud_de_geojoined.rds")
+geo_data <- acs_hud_de_geojoined
+geo_data_nogeometry <- geo_data %>% 
+  st_drop_geometry()
+
+advoc_table <- geo_data_nogeometry %>% 
+  #mutate(reported_labels=ifelse(number_reported==-4,"Less than 10 Households","NA")) %>%
+  #mutate(number_reported =replace(number_reported, number_reported<0, 0) ) %>%
+  rowwise() %>%
+  mutate(above30 = sum(rent_30E, rent_35E, rent_40E,rent_50E),
+         above50=rent_50E) %>%
+  group_by(GEOID) %>%
+  summarize(reported_HUD=sum(number_reported),rent_above30=sum(above30),rent_above50=sum(above50)) %>%
+  dplyr::rename(
+    '# Households receiving assisstance'=reported_HUD,
+    '# Households spending above 30% of income on rent'=rent_above30,
+    '# Households spending above 50% of income on rent'=rent_above50,
+  )
--- app/server.R
+++ app/server.R
@@ -3,12 +3,15 @@ library(tidyverse)
 library(plotly)
 library(sf)
 
+source("Scripts/advocates.R")
+source("Scripts/county.R")
+
 # Load Data
 acs_hud_de_geojoined <- read_rds("acs_hud_de_geojoined.rds")
 geo_data <- acs_hud_de_geojoined
 geo_data_nogeometry <- geo_data %>% 
     st_drop_geometry()
-
+#print(length(unique(geo_data_nogeometry$GEOID)))
 # Reshape the dataset into the long format for summarizing 
 geo_long <- geo_data_nogeometry %>%
     mutate(number_not_using = eligible_renters - number_reported) %>%
@@ -34,102 +37,6 @@ de_summary_table <- geo_data_nogeometry %>%
     group_by(GEOID) %>% 
     mutate(tot = number_reported)
 
-data_county <- geo_data_nogeometry %>%
-    filter(number_reported > 0) %>%
-    mutate(county = substr(GEOID, 3, 5)) %>%
-    rowwise() %>% 
-    mutate(above30 = sum(rent_30E, rent_35E, rent_40E),
-           above50 = rent_50E) %>%
-    group_by(county) %>%
-    summarize(reported_HUD = sum(number_reported),
-              rent_above30 = sum(above30),
-              rent_above50 = sum(above50))
-
-# Number of households spending above 30% and 50% of hh_income on rent.
-number_county_30 = data_county %>%  select(reported_HUD,rent_above30,county) %>%
-    dplyr::rename(
-        'Household applied for Section 8 assisstance'=reported_HUD,
-        'Household spending above 30% of income on rent'=rent_above30) %>%
-    gather(Category, count, -c(county)) %>%
-    ## na.rm = TRUE ensures all values are NA are taken as 0
-    ggplot(aes(x=county,y=count))+
-    geom_bar(aes(fill=Category),   # fill depends on cond2
-             stat="identity",
-             colour="black",    # Black outline for all
-             position=position_dodge())+
-    ylab("Number of households")+
-    xlab("County")+
-    labs(color = "Percentage of hh_income spent on rent")+
-    ggtitle("Number of households soending above 50% of household income on rent")
-
-number_county_50 = data_county %>%  select(reported_HUD,rent_above50,county) %>%
-    dplyr::rename(
-        'Household applied for Section 8 assisstance'=reported_HUD,
-        'Household spending above 50% of income on rent'=rent_above50) %>%
-    gather(Category, count, -c(county)) %>%
-    ## na.rm = TRUE ensures all values are NA are taken as 0
-    ggplot(aes(x=county,y=count))+
-    geom_bar(aes(fill=Category),   # fill depends on cond2
-             stat="identity",
-             colour="black",    # Black outline for all
-             position=position_dodge())+
-    ylab("Number of households")+
-    xlab("County")+
-    labs(color = "Percentage of hh_income spent on rent")+
-    ggtitle("Number of households soending above 50% of household income on rent")
-
-# Proportion of households spending above 30% and 50% of hh_income on rent and not receiving assitance.
-prop_county_30 = data_county %>% mutate(rent_above30=(rent_above30-reported_HUD)/rent_above30) %>%
-    select(county,rent_above30) %>%
-    dplyr::rename(
-        'Households spending above 30% of income on rent'=rent_above30) %>%
-    gather(Category, count, -c(county)) %>%
-    ## na.rm = TRUE ensures all values are NA are taken as 0
-    ggplot(aes(x=county,y=count))+
-    geom_bar(aes(fill=Category),   # fill depends on cond2
-             stat="identity",
-             colour="black",    # Black outline for all
-             position=position_dodge())+
-    ylab("Proportion of households NOT receiving assistance")+
-    xlab("County")+
-    labs(color = "Percentage of hh_income spent on rent")+
-    theme(legend.position="top") +
-    ggtitle("")
-
-
-prop_county_50 = data_county %>% mutate(rent_above50=(rent_above50-reported_HUD)/rent_above50) %>%
-    select(county,rent_above50) %>%
-    dplyr::rename(
-        'Households spending above 50% of income on ren'=rent_above50
-    ) %>%
-    gather(Category, count, -c(county)) %>%
-    ## na.rm = TRUE ensures all values are NA are taken as 0
-    ggplot(aes(x=county,y=count))+
-    geom_bar(aes(fill=Category),   # fill depends on cond2
-             stat="identity",
-             colour="black",    # Black outline for all
-             position=position_dodge())+
-    ylab("Proportion of households NOT receiving assistance")+
-    xlab("County")+
-    labs(color = "Percentage of hh_income spent on rent")+
-    theme(legend.position="top") +
-    ggtitle("")
-
-
-# Data for Advocates Tab
-#Statistics for GEOIDs
-advoc_table <- geo_data_nogeometry %>%
-    filter(number_reported > 0 ) %>%
-    rowwise() %>%
-    mutate(above30 = sum(rent_30E, rent_35E, rent_40E),
-           above50=rent_50E) %>%
-    group_by(GEOID) %>%
-    summarize(reported_HUD=sum(number_reported),rent_above30=sum(above30),rent_above50=sum(above50)) %>%
-    dplyr::rename(
-        '# Households receiving assisstance'=reported_HUD,
-        '# Households spending above 30% of income on rent'=rent_above30,
-        '# Households spending above 50% of income on rent'=rent_above50,
-    )
 
 
 # Labels
