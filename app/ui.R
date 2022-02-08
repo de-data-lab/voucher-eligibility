aed324f9dc113b87c66bdcb169cf317802b39ed1
Nami Sunami <naoyuki.sunami@gmail.com>
2022-02-08 15:05:54 -0500

    Remove the counties panel from the main ui

--- app/ui.R
+++ app/ui.R
@@ -5,7 +5,6 @@ library(plotly)
 # Load tab-panels
 source("home_panel.r")
 source("advocates_panel.r")
-source("counties_panel.R")
 source("methods_panel.R")
 
 navbarPage(
@@ -14,7 +13,6 @@ navbarPage(
         tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
     ),
     home_panel, # Add a tab panel for home
-    counties_panel,
     advocates_panel,
     methods_panel,
     footer = tags$div(class = "footer",
