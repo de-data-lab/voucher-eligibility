36e4f0275e8f8e7ac8ee905d58650f39e3b6f275
Nami Sunami <naoyuki.sunami@gmail.com>
2022-02-15 16:45:09 -0500

    Remove numpy dependency

--- app/server.R
+++ app/server.R
@@ -4,7 +4,7 @@ library(plotly)
 library(sf)
 library(reticulate)
 
-PYTHON_DEPENDENCIES = c('pip', 'numpy', 'censusgeocode')
+PYTHON_DEPENDENCIES = c('pip', 'censusgeocode')
 
 source("scripts/plotly_settings.R")
 source("scripts/advocates.R")
