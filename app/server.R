6080dc090670181700031ee2d032eed21f12ff76
mehak25 <47729138+mehak25@users.noreply.github.com>
2022-02-16 16:06:21 -0500

    Adding text observe event

--- app/server.R
+++ app/server.R
@@ -257,17 +257,16 @@ shinyServer(function(input, output, session) {
     current_GEOID <- eventReactive(input$address_search,
                                    {tryCatch(
                                        {
-                                           geoid <- return_geoid(input$address)
-                                           id=shape %>% filter(GEOID==geoid)
-                                           clicked_ids$Clicks <- c(clicked_ids$Clicks, id$NAMELSAD)
-                                           clicked_ids$Clicks <- unique(clicked_ids$Clicks)
-                                           sub <- shape %>% filter(NAMELSAD %in% (clicked_ids$Clicks))
-                                           return geoid
+                                           return_geoid(input$address)
+                                           # id=shape %>% filter(GEOID==geoid)
+                                           # clicked_ids$Clicks <- c(clicked_ids$Clicks, id$NAMELSAD)
+                                           # clicked_ids$Clicks <- unique(clicked_ids$Clicks)
+                                           # sub <- shape %>% filter(NAMELSAD %in% (clicked_ids$Clicks))
+                                           # return geoid
                                        },
                                     error = function(cond){
                                         "No GEOID found"
                                         }
- 
                                        )
                                        })
     output$current_GEOID <-  renderText({current_GEOID()})
