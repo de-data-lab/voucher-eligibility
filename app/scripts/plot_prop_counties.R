d6e3d06320e71bddf49d8b590af75912ebfb2279
Nami Sunami <naoyuki.sunami@gmail.com>
2022-02-17 11:43:55 -0500

    Add the testing plotly code

--- app/scripts/plot_prop_counties.R
+++ app/scripts/plot_prop_counties.R
@@ -33,3 +33,38 @@ plot_prop_counties <- function(.data){
     
     return(out_plot)
 }
+
+
+prop_counties_data <- .data %>%
+    group_by(county_name, labels) %>%
+    summarise(count = sum(value, na.rm = TRUE)) %>%
+    mutate(prop = count / sum(count))
+
+prop_counties_data %>%
+plot_ly(x = ~prop,
+        y = ~county_name,
+        color = ~labels,
+        type = "bar",
+        orientation = "h",
+        hovertemplate = "%{x} Eligible Families") %>%
+    layout(barmode = "stack",
+           xaxis = list(title = "",
+                        tickformat = ".0%"),
+           yaxis = list(title = "")) %>%
+    add_annotations(yref = "y",
+                    y = ~county_name,
+                    xref = "paper",
+                    x = ~prop ,
+                    text = ~rev(
+                        paste0(round(prop * 100, 2),
+                               "%",
+                               "<br>",
+                                  labels)
+                        ),
+                    showarrow = FALSE) %>%
+    plotly_disable_zoom() %>%
+    plotly_hide_modebar() %>%
+    plotly_legend_top_right()
+
+
+
