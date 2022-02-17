63efb36349c40964bdcf919550cc864276a0e271
mehak25 <47729138+mehak25@users.noreply.github.com>
2022-02-16 22:22:18 -0500

    Adding text observe event

--- app/server.R
+++ app/server.R
@@ -275,6 +275,18 @@ shinyServer(function(input, output, session) {
                                        {
                                            found_GEOID$ids <- return_geoid(input$address)
                                            output$current_GEOID <-  renderText({found_GEOID$ids})
+                                           sub <- shape %>% filter(GEOID %in% (found_GEOID$ids))
+                                           clicked_ids$Clicks <- c(clicked_ids$Clicks, sub$NAMELSAD) # name when clicked, id when unclicked
+                                           clicked_ids$Clicks <- unique(clicked_ids$Clicks)
+                                           leafletProxy("advocmap") %>% addTiles() %>%
+                                               addPolygons(data=sub,
+                                                           fillColor = "#b30000",color = "#2b8cbe",opacity = 1,weight=2,
+                                                           fillOpacity = 0.8, smoothFactor = 0.5,
+                                                           highlight=highlightOptions(fillOpacity = 0.8,
+                                                                                      color = "#b30000",
+                                                                                      weight = 2,
+                                                                                      bringToFront=TRUE),
+                                                           label= ~NAMELSAD, layerId = ~NAMELSAD)
                                        },
                                        error = function(cond){
                                            found_GEOID$ids <- "No GEOID found"
