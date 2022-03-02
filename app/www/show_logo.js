d9dc987a563c168d76c9895799378d456739a49a
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-02 15:55:19 -0500

    Open a new window when clicking the link to TI

--- app/www/show_logo.js
+++ app/www/show_logo.js
@@ -1,3 +1,3 @@
 var header = $('.navbar > .container-fluid');
 
-header.append('<div style=\"float:right\"><a href=\"https://techimpact.org/\"><img src=\"techimpact_logo.png\" alt=\"Tech Impact Logo\" style=\"float:right;width:9em;padding-top:15px;\"></a></div>');
+header.append('<div style=\"float:right\"><a href=\"https://techimpact.org/\" target=\"_blank\"><img src=\"techimpact_logo.png\" alt=\"Tech Impact Logo\" style=\"float:right;width:9em;padding-top:15px;\"></a></div>');
