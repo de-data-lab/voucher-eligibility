b018b807371e3e2cc3fa915698af21d10efe6dc5
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-07 15:22:06 -0500

    Create a title for the count bar chart

--- app/counties.R
+++ app/counties.R
@@ -7,14 +7,16 @@ counties_div <- tags$div(
                                "New Castle County has the largest number of
              potentially-eligible families")
              ),
-             plotlyOutput("number_county"),
-             tags$div(class = "select-threshold",
-                      radioGroupButtons("selectedNumber", 
-                                        label = "Focusing on families with rent spending:",
-                                        choices = c("30%+ of income" = "30",
-                                                    "50%+ of income" = "50"),
-                                        selected='30')
-             )
+             tags$div(class = "select-threshold plot-title",
+                                tags$div("Number of "),
+                                selectInput("selectedNumber", 
+                                            label = NULL,
+                                            choices = c("rent-burdened" = "30",
+                                                        "severely rent-burdened" = "50"),
+                                            selected = "30"),
+                                tags$div("families")
+             ),
+             plotlyOutput("number_county")
     ),
     tags$div(class = "main-point-container",
              tags$div(class = "main-heading-container", 
