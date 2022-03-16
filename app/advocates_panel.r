ca69d6b9cce4ca0529dfe96134463f50e44a2c0f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-03-16 15:03:33 -0400

    Revise the CTA on the explore page

--- app/advocates_panel.r
+++ app/advocates_panel.r
@@ -35,11 +35,14 @@ advocates_panel <- tabPanel(
                  tags$div(class = "explore-title-container",
                           tags$div(class = "main-point bold",
                                    "How is your neighborhood doing?"),
-                          tags$div(class = "sub-point",
-                                   "Find out what percentage of households in your neighbourhood
-             are receiving Housing Choice Voucher and
-              what percentage of households are spending
-             more than 30% and 50% of their income on rent")
+                          tags$div(class = "explore-subtitle",
+                                   icon("mouse-pointer"),
+                                   "Select census tracts on the map",
+                                   tags$br(),
+                                   "or",
+                                   tags$br(),
+                                   icon("edit"),
+                                   "Enter your address:"),
                           tags$div(class = "address-input-container",
                                    searchInput("address", label = NULL, placeholder = "Enter your address",
                                                btnSearch = icon("search")),
