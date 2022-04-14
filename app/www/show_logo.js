a53e6bd7d1ac18639a54fdab2a930823344dc74f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-04-14 16:05:13 -0400

    Remove inline styles

--- app/www/show_logo.js
+++ app/www/show_logo.js
@@ -1,3 +1,3 @@
 var header = $('.navbar > .container-fluid');
 
-header.append('<div style=\"float:right\"><a href=\"https://techimpact.org/\" target=\"_blank\"><img src=\"techimpact_logo.png\" alt=\"Tech Impact Logo\" style=\"float:right;width:9em;padding-top:15px;\"></a></div>');
+header.append('<span class=\"logo-container\""><a href=\"https://techimpact.org/\" target=\"_blank\"><img src=\"techimpact_logo.png\" alt=\"Tech Impact Logo\" class=\"logo-image img-fluid\"></a></span>');
