88c5127b2347045d9cda5b872b2e6a63cf50fd0d
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-16 11:21:06 -0400

    Remove unnecessary splicing

--- app/R/plotCard.R
+++ app/R/plotCard.R
@@ -1,7 +1,7 @@
-plotCard <- function(main_text = list(), secondary_text = list(), plot_content = list()){
+plotCard <- function(main_text, secondary_text, plot_content){
     div(class = "main-point-container",
         div(class = "main-heading-container", 
-            div(class = "main-point", !!!main_text)),
-        div(class = "select-county plot-title", !!!secondary_text),
-        !!!plot_content)
+            div(class = "main-point", main_text)),
+        div(class = "select-county plot-title", secondary_text),
+        plot_content)
 }
