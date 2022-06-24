e9ba0dd2e751280fd3a2fad54d6156090dd2cc42
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-24 10:39:08 -0400

    Avoid recomputing

--- app/R/plot_counts_counties.R
+++ app/R/plot_counts_counties.R
@@ -6,59 +6,48 @@ library(sf)
 library(RColorBrewer)
 
 plot_counts_counties <- function(.data, cutoff){
-    # Create by-county table (data object inherited from server.R)
+    # Create a summary table (one row representing a county)
     data_county <- .data %>%
         filter(number_reported > 0) %>%
-        mutate(county = substr(GEOID, 3, 5)) %>%
-        mutate(county=replace(county, county=='001', 'Kent')) %>%
-        mutate(county=replace(county, county=='003', 'New Castle')) %>%
-        mutate(county=replace(county, county=='005', 'Sussex'))  %>%
-        rowwise() %>% 
-        #mutate(above30 = sum(rent_30E, rent_35E, rent_40E,rent_50E),
-        #       above50 = rent_50E) %>%
-        mutate(above30 = eligible_renters,
-               above50 = sum(rent_50_10kE,rent_50_20kE,rent_50_35kE,rent_50_50kE,rent_50_75kE)) %>%
-        group_by(county) %>%
-        summarize(reported_HUD = sum(number_reported),
-                  rent_above30 = sum(above30),
-                  rent_above50 = sum(above50)) 
-    
-    # Determine the labels and data depending on the 30/50 cutoff values
+        group_by(county_name) %>%
+        summarize(number_reported = sum(number_reported),
+                  rent_above30 = sum(rent_above30),
+                  rent_above50 = sum(rent_above50)) 
+
+    # Determine the labels depending on the 30/50 cutoff values
     if(cutoff == 30){
         family_text <- "rent-burdened famillies"
         category_label <- "Spending 30%+ income on rent"
-        
-        plot_data <- data_county %>%  
-            select(reported_HUD, rent_above30, county) %>%
-            dplyr::rename(
-                'Receiving Voucher' = reported_HUD,
-                'Spending 30%+ income on rent' = rent_above30) %>%
-            gather(Category, count, -c(county))
-        }
-    
+    }
     if(cutoff == 50){
         family_text <- "severely rent-burdened famillies"
         category_label <- "Spending 50%+ income on rent"
-        
-        plot_data <- data_county %>% 
-            select(reported_HUD, rent_above50, county) %>%
-            dplyr::rename(
-                'Receiving Voucher' = reported_HUD,
-                'Spending 50%+ income on rent' = rent_above50) %>%
-            gather(Category, count, -c(county))
     }
     
+    # Construct a long-format data for the plot
+    # (one row representing a category-county pair)
+    plot_data <- data_county %>% 
+        select(number_reported, rent_above30, rent_above50, county_name) %>%
+        pivot_longer(cols = c(number_reported, rent_above30, rent_above50),
+                     names_to = "category",
+                     values_to = "count") %>%
+        # Create a column for labeling
+        mutate(category_label = recode(category,
+                                       "number_reported" = "Receiving Voucher",
+                                       "rent_above50" = "Spending 50%+ income on rent",
+                                       "rent_above30" = "Spending 30%+ income on rent"))
+
     # Create the output Plotly plot
-    out_plot <- plot_ly() %>%
-        add_bars(data = plot_data %>% filter(Category == "Receiving Voucher"),
-                 x = ~count, y = ~county,
+    plot_ly() %>%
+        add_bars(data = plot_data %>% filter(category_label == "Receiving Voucher"),
+                 x = ~count, y = ~county_name,
                  marker = list(color = "#66C2A5"),
                  name = "Receiving Voucher",
                  hovertemplate = str_wrap_br(
                      paste0("In %{y} County, %{x:,} families are receiving a voucher <extra></extra>"),
                      width = 30)) %>%
-        add_bars(data = plot_data %>% filter(Category == category_label),
-                 x = ~count, y = ~county,
+        add_bars(data = plot_data %>% filter(category_label == category_label),
+                 x = ~count, y = ~county_name,
                  marker = list(color = "#FC8D62"),
                  name = str_to_title(family_text),
                  hovertemplate = str_wrap_br(
@@ -82,11 +71,4 @@ plot_counts_counties <- function(.data, cutoff){
                margin = list(pad = 15),
                paper_bgcolor = "transparent") %>% 
         format_plotly()
-    
-    return(out_plot)
 }
-
-
-
-
-
