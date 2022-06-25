7ef76e0afef2c5dcb1adc2ea648e9487ff13656f
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-25 19:14:13 -0400

    Replace `tags$div` with `div`

--- app/R/explore_panel.R
+++ app/R/explore_panel.R
@@ -8,15 +8,15 @@ explore_panel <- tabPanel(
     title = "Explore Your Neighborhood",
     fluidRow(
         class = "explore-page-container",
-        tags$div(class = "map-container",
-                 tags$div(
-                     class = "map-header-container",
-                     tags$div(
-                         class = "clear-all-button", 
-                         actionButton("clear", "Clear All"))
-                 ),
-                 map_UI("explore"),
-                 tags$script("
+        div(class = "map-container",
+            div(
+                class = "map-header-container",
+                div(
+                    class = "clear-all-button", 
+                    actionButton("clear", "Clear All"))
+            ),
+            map_UI("explore"),
+            tags$script("
                     $(document).ready(function() {    
                       setTimeout(function() {
                 
@@ -32,51 +32,51 @@ explore_panel <- tabPanel(
                       }, 100);
                     })
                   ")),
-        tags$div(class = "explore-contents-container", 
-                 tags$div(class = "explore-title-container",
-                          tags$div(class = "main-point bold",
-                                   "How is your neighborhood doing?"),
-                          tags$div(class = "explore-subtitle",
-                                   icon("mouse-pointer"),
-                                   "Select census tracts on the map",
-                                   tags$br(),
-                                   "or",
-                                   tags$br(),
-                                   icon("edit"),
-                                   "Look up a census tract using your address"),
-                          tags$div(class = "address-input-container",
-                                   searchInput("address", label = NULL, placeholder = "Street Address",
-                                               btnSearch = icon("search")),
-                                   tags$div(class = "address-message",
-                                            textOutput("address_message")
-                                   )
-                          )
-                 ),
-                 compare_to_de_plot_UI("compare_to_de"),
-                 tags$div(class = "bar-footnote-container",
-                          tags$span(class = "tooltip-span",
-                                    icon(name = "question-circle"), "What do \"rent-burdnened\" and \"severely rent-burdened\" mean?",
-                                    tags$span(class = "tooltip-text",
-                                              "Families spending more than 30% of their income on
+        div(class = "explore-contents-container", 
+            div(class = "explore-title-container",
+                div(class = "main-point bold",
+                    "How is your neighborhood doing?"),
+                div(class = "explore-subtitle",
+                    icon("mouse-pointer"),
+                    "Select census tracts on the map",
+                    tags$br(),
+                    "or",
+                    tags$br(),
+                    icon("edit"),
+                    "Look up a census tract using your address"),
+                div(class = "address-input-container",
+                    searchInput("address", label = NULL, placeholder = "Street Address",
+                                btnSearch = icon("search")),
+                    div(class = "address-message",
+                        textOutput("address_message")
+                    )
+                )
+            ),
+            compare_to_de_plot_UI("compare_to_de"),
+            div(class = "bar-footnote-container",
+                tags$span(class = "tooltip-span",
+                          icon(name = "question-circle"), "What do \"rent-burdnened\" and \"severely rent-burdened\" mean?",
+                          tags$span(class = "tooltip-text",
+                                    "Families spending more than 30% of their income on
                                               rent are considered \"rent-burdened\". 
                                               If they are spending more than 50%, they are considered
                                               \"severely rent-burdened\"."))
-                 ),
-                 rank_plot_UI("rank_plot"),
-                 tags$div(class = "advoc-table-container",
-                          tags$div(class = "advoc-table",
-                                   tableOutput("advoc_table")
-                          ),
-                          tags$div(class = "table-footnote",
-                                   downloadButton("downloadData", "Download")),
-                          tags$div(class = "down-footnote","(Dowloads data from above table)"),
-                          tags$div(class = "table-footnote",
-                                   "The number of households appears as 10 when there are 10 or less 
+            ),
+            rank_plot_UI("rank_plot"),
+            div(class = "advoc-table-container",
+                div(class = "advoc-table",
+                    tableOutput("advoc_table")
+                ),
+                div(class = "table-footnote",
+                    downloadButton("downloadData", "Download")),
+                div(class = "down-footnote","(Dowloads data from above table)"),
+                div(class = "table-footnote",
+                    "The number of households appears as 10 when there are 10 or less 
              households in a given cell.",
-                                   tags$br(),
-                                   downloadLink("downloadAll", "Download All Data"))
-                 ),
-                 includeHTML("CTA.html")
+                    tags$br(),
+                    downloadLink("downloadAll", "Download All Data"))
+            ),
+            includeHTML("CTA.html")
         )
     )
 )
