f67768006d6d302514af21725f40c3d8f2cf95ae
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-25 19:16:45 -0400

    Start a Shiny module for address search

---
+++ app/R/address_search.R
@@ -0,0 +1,13 @@
+# Address search Shiny module
+
+address_search_UI <- function(id) {
+  tagList(
+    
+  )
+}
+
+address_search_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    
+  })
+}
