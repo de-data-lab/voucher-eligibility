24ae159e7a1253f6d08a8723ba31b97c4f2aa059
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-25 19:08:21 -0400

    Add a helper function to get a percent summaries

---
+++ app/R/get_pct_table.R
@@ -0,0 +1,25 @@
+# Get the table showing the percentage of the rent-burdened, 
+# severely rent-burdened, and receiving voucher groups
+# The resulting output will be a summary table where
+# one row represents a category
+
+get_pct_table <- function(.data){
+    # Summarize across variables
+    .data <- .data %>% 
+        group_by() %>%
+        summarise(across(
+            c(number_reported,
+              rent_above30,
+              rent_above50,
+              tot_hh),
+            ~sum(., na.rm = TRUE)
+        ))
+    # Get the percentage and drop the total counts
+    .data <- .data %>%
+        mutate(across(-tot_hh, ~(. / tot_hh))) %>%
+        select(-tot_hh)
+    # Pivot to create a table where one row represents a level-type pair
+    .data %>%
+        pivot_longer(everything(), values_to = "prop") %>%
+        mutate(pct = prop * 100)
+}
