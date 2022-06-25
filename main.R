c42d2fa49ec1604b175f9528f0ad0aa7c3c00902
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-24 20:21:31 -0400

    Update data

--- main.R
+++ main.R
@@ -6,3 +6,20 @@ source(here("src/download_hud_datasets.R"))
 source(here("src/join_acs_hud.R"))
 source(here("src/add_geometry_to_ACS_HUD.R"))
 source(here("src/add_columns.R"))
+source(here("src/create_summary_tables.R"))
+
+
+acs_hud_de_geojoined <- read_rds(here("data/processed/acs_hud_de_geojoined.rds"))
+DE_summary_receiving_voucher <- acs_hud_de_geojoined %>% 
+    st_drop_geometry() %>%
+    create_DE_summary_table() 
+
+
+write_rds(DE_summary_receiving_voucher, 
+          here("app/data/acs_hud_de_geojoined_summary.rds"))
+
+
+acs_hud_de_geojoined_voucher_count_long <- acs_hud_de_geojoined %>%
+    create_long_count_table()
+write_rds(acs_hud_de_geojoined_voucher_count_long,
+          here("app/data/acs_hud_de_geojoined_count_long.rds"))
--- src/add_columns.R
+++ src/add_columns.R
@@ -9,7 +9,40 @@ acs_hud_de_geojoined <- acs_hud_de_geojoined %>%
 acs_hud_de_geojoined <- acs_hud_de_geojoined %>%
     arrange(as.numeric(tract))
 
+# Create a custom table
+acs_hud_de_geojoined <- acs_hud_de_geojoined %>%
+    # Create a sum of eligible renters
+    rowwise() %>%
+    mutate(rent_above30 = eligible_renters,
+           rent_above50 = sum(rent_50_10kE, rent_50_20kE, rent_50_35kE, rent_50_50kE, rent_50_75kE),
+           reported_HUD = number_reported,
+           tot_hh = tot_hhE) %>%
+    ungroup()
+
+# Calculate the proportion of renters under each categories
+acs_hud_de_geojoined <- acs_hud_de_geojoined %>%
+    mutate(prop_above30 = rent_above30 / tot_hh,
+           prop_above50 = rent_above50 / tot_hh,
+           prop_reported_HUD = (reported_HUD / tot_hh)) %>%
+    # calculate percents 
+    mutate(across(c(prop_above30, prop_above50, prop_reported_HUD),
+                  .fns = ~. * 100,
+                  .names = "pct_{.col}"))
+
+# Dictionary of counties and keys
+county_list <- c(
+    "all" = "Delaware",
+    "001" = "Kent County",
+    "003" = "New Castle County",
+    "005" = "Sussex County")
+
+# Update the county names
+acs_hud_de_geojoined <- acs_hud_de_geojoined %>%
+    mutate(county_name = str_remove(recode(COUNTYFP, !!!county_list),
+                                    " County"))
+
+
 # Save to the main project
 write_rds(acs_hud_de_geojoined, here("data/processed/acs_hud_de_geojoined.rds"))
 # Save to the app
-write_rds(acs_hud_de_geojoined, here("app/acs_hud_de_geojoined.rds"))
+write_rds(acs_hud_de_geojoined, here("app/data/acs_hud_de_geojoined.rds"))
