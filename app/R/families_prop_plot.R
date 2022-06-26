5b0950ca0cf89086aa085705335f163a097967b0
Nami Sunami <naoyuki.sunami@gmail.com>
2022-06-26 18:31:13 -0400

    Clean indents

--- app/R/families_prop_plot.R
+++ app/R/families_prop_plot.R
@@ -4,14 +4,14 @@ source("R/select_rent_burden.R")
 
 families_prop_plot_UI <- function(id) {
     plot_card(main_text = "Sussex County is struggling the most to help Delaware families", 
-             secondary_text = select_rent_burden(id, prefix = NULL),
-             plot_content = plotlyOutput(NS(id, "prop_counties")))
+              secondary_text = select_rent_burden(id, prefix = NULL),
+              plot_content = plotlyOutput(NS(id, "prop_counties")))
 }
 
 familiesPropPlotServer <- function(id, .data_long) {
-  moduleServer(id, function(input, output, session) {
-      output$prop_counties <- renderPlotly({
-              plot_prop_counties(.data_long, input$threshold)
-      })
-  })
+    moduleServer(id, function(input, output, session) {
+        output$prop_counties <- renderPlotly({
+            plot_prop_counties(.data_long, input$threshold)
+        })
+    })
 }
