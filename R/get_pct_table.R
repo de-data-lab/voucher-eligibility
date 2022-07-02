a2da9a0228536d0cfe52902f1659c2c57419505d
Nami Sunami <naoyuki.sunami@gmail.com>
2022-07-01 20:50:10 -0400

    Update the object documentation

--- R/get_pct_table.R
+++ R/get_pct_table.R
@@ -1,16 +1,22 @@
-# Get the table showing the percentage of the rent-burdened, 
-# severely rent-burdened, and receiving voucher groups
-# The resulting output will be a summary table where
-# one row represents a category
-
-#' Get a summary table of rent-burdened, severely rent-burdened, and voucher participant groups.
+#' Get a summary table of percentages from a wide dataset 
+#' 
+#' `get_pct_table` gets a summary table of percentages of rent-burdened,
+#'  severely rent-burdened, and voucher participants from the total households
 #'
-#' @param .data 
+#' @param .data A wide-format data frame representing one row per census tract. 
+#' The data frame needs to have the columns: "number_reported", "rent_above30", 
+#' "rent_above50", and "tot_hh"
 #'
-#' @return
+#' @return A summary table of percentages (pct) and proportions (prop).
+#' One row represents a household category (rent-burdened, severely burdened, and voucher participants)
+#'
+#' @examples 
+#' acs_hud_de_geojoined %>%
+#'  st_drop_geometry() %>%
+#'  get_pct_table()
+#' 
 #' @export
-#'
-#' @examples
+#'  
 get_pct_table <- function(.data){
     # Summarize across variables
     .data <- .data %>% 
